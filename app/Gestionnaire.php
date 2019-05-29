974e703959bdc7611f58091b6d940e99f5afa1bf
dev <cagoulartstudios@gmail.com>
2019-05-29 20:30:20 +0000

    du 29/05/2019

---
+++ app/Gestionnaire.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ * Date: Wed, 29 May 2019 20:26:07 +0000.
+ */
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+/**
+ * Class Gestionnaire
+ * 
+ * @property int $id
+ * @property string $uuid
+ * @property string $matricule
+ * @property int $users_id
+ * @property string $deleted_at
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * 
+ * @property \App\User $user
+ * @property \Illuminate\Database\Eloquent\Collection $clients
+ *
+ * @package App
+ */
+class Gestionnaire extends Eloquent
+{
+	use \Illuminate\Database\Eloquent\SoftDeletes;
+
+	protected $casts = [
+		'users_id' => 'int'
+	];
+
+	protected $fillable = [
+		'uuid',
+		'matricule',
+		'users_id'
+	];
+
+	public function user()
+	{
+		return $this->belongsTo(\App\User::class, 'users_id');
+	}
+
+	public function clients()
+	{
+		return $this->hasMany(\App\Client::class, 'gestionnaires_id');
+	}
+}
---
+++ app/Role.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ * Date: Wed, 29 May 2019 20:26:07 +0000.
+ */
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+/**
+ * Class Role
+ * 
+ * @property int $id
+ * @property string $uuid
+ * @property string $name
+ * @property string $deleted_at
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * 
+ * @property \Illuminate\Database\Eloquent\Collection $users
+ *
+ * @package App
+ */
+class Role extends Eloquent
+{
+	use \Illuminate\Database\Eloquent\SoftDeletes;
+
+	protected $fillable = [
+		'uuid',
+		'name'
+	];
+
+	public function users()
+	{
+		return $this->hasMany(\App\User::class, 'roles_id');
+	}
+}
